55e428c7310fe83894c4be546062ced662c34621
roblescristian4722 <roblescristian4722@gmail.com>
2022-03-19 22:40:57 -0600

    single monitor

--- launch.sh
+++ launch.sh
@@ -6,7 +6,16 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# echo "---" | tee -a /tmp/polybar1.log
+# polybar base #2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload base &
+  done
+else
+  polybar --reload base &
+fi
 
 echo "Bars launched..."

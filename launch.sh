ddc7c4c0d0de4dc1ed0efa4a82627c2ce365f34e
roblescristian4722 <roblescristian4722@gmail.com>
2023-08-19 20:58:15 -0600

    Base and secondary are called only for main and secondary monitors

--- launch.sh
+++ launch.sh
@@ -11,16 +11,14 @@ sleep 0.5
 # polybar base #2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
 
-# MONITOR=$m polybar --reload base &
-sleep 0.2
-
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1) - 1; do
-    MONITOR=$m polybar --reload secondary &
-    sleep 0.2
-  done
+    MONITOR=$m polybar --reload base &
+    for m in $(xrandr --query | grep " connected" | awk -F ' ' 'NR > 1 {print $1}'); do
+        MONITOR=$m polybar --reload secondary &
+        sleep 0.2
+    done
 else
-  polybar --reload base &
+    polybar --reload base &
 fi
 
 echo "Bars launched..."

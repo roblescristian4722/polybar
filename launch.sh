0a8a466097ff90e3ab3bc5d683ebe3485b97e599
roblescristian4722 <roblescristian4722@gmail.com>
2023-08-19 20:38:16 -0600

    font and missing scripts issues fixed

--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 
 # Terminate already running bar instances
 killall -q polybar
+sleep 0.5
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
@@ -10,9 +11,13 @@ killall -q polybar
 # polybar base #2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
 
+# MONITOR=$m polybar --reload base &
+sleep 0.2
+
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload base &
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1) - 1; do
+    MONITOR=$m polybar --reload secondary &
+    sleep 0.2
   done
 else
   polybar --reload base &
